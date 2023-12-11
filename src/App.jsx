6c21dfb3596892486c3165450cbe9aa0abe99bd6
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-12-11 11:51:03 +0000

    added results component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
+    <StrictMode>
   <div>
     <h1>Adopt Me!</h1>
     <SearchParams />
   </div>
+  </StrictMode>
 )};
 
 const container = document.getElementById("root");
