848e874347b6aacce3aa16d4fb70713ade629eee
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-12-11 16:45:11 +0000

    useparams added

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,32 @@
+import { useParams } from "react-router-dom";
+import fetchPet from "./fetchPet";
+import { useQuery } from "@tanstack/react-query";
+
 const Details = () => {
-    return <h2>hi!</h2>;
-  };
-  
-  export default Details;
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
+          <button>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
